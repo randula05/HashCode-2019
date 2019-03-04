abad330aa62b12c996687cb8665845b488de55ff
Randula Wickramasuriya <randula05@gmail.com>
2019-03-04 14:42:35 +0530

    done no order

--- hashcode19.cpp
+++ hashcode19.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 typedef struct photo{
-    int id;
+    int id[2];
     char hv;
     int tagCount;
     int v;
@@ -13,12 +13,15 @@ typedef struct photo{
 photo *start=NULL;
 
 int main(){
+    // Input file
     ifstream file("a_example.txt");
+
+    // Assign variables
     int n;
     file>>n;
     for(int i=0;i<n;i++){
         photo *p=new photo;
-        p->id=i;
+        p->id[0]=i;
         file>>p->hv>>p->tagCount;
         p->v=-1;
         string s;
@@ -39,9 +42,65 @@ int main(){
             ptr->next=p;
         }
     }
+
+    // Arrange photo nodes
+    int merge=0,count=0;
     photo *ptr=start;
+    photo *temp;
     while(ptr!=NULL){
-        cout<<ptr->id<<ptr->hv<<ptr->tagCount<<" "<<ptr->tags[0]<<ptr->tags[1]<<endl;
+        if(ptr->hv=='V'){
+            if(merge==0){
+                ptr->hv='H';
+                temp=ptr;
+                count++;
+            }else{
+                temp->id[1]=ptr->id[0];
+                int tempCount=temp->tagCount;
+                for(int i=0;i<ptr->tagCount;i++){
+                    int f=0;
+                    for(int j=0;j<tempCount;j++){
+                        //cout<<ptr->tags[i]<<temp->tags[j]<<endl;
+                        if(ptr->tags[i]==temp->tags[j]){
+                            f=1;
+                            break;
+                        }
+                        if(f==0 && j==tempCount-1){
+                            temp->tagCount++;
+                            temp->v++;
+                            temp->tags[temp->v]=ptr->tags[i];
+                            //cout<<temp->tags[temp->v]<<endl;
+                        }
+                        //cout<<temp->tagCount<<endl;
+                    }
+                }
+                cout<<endl;
+                /* for(int i=0;i<temp->tagCount;i++){
+                    for(int j=0;j<ptr->tagCount;j++){
+                        cout<<temp->tags[i]<<" "<<ptr->tags[j]<<endl;
+                        
+                    }
+                } */
+            }
+            merge=!merge;
+        }else
+            count++;
+        ptr=ptr->next;
+    }
+
+
+
+    // Test
+    cout<<endl;
+    cout<<count<<endl;
+    ptr=start;
+    while(ptr!=NULL){
+        if(ptr->hv=='H'){
+            cout<<ptr->id[0];
+            if(ptr->id[1]){
+                cout<<" "<<ptr->id[1];
+            }
+            cout<<endl;
+        }
         ptr=ptr->next;
     }
     
